116017406afcde1b5f763e0b4f54e3e9e16d299d
Noskov Alexey <vokson@yandex.ru>
2020-10-18 12:25:12 +0300

    Add Exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Exception;
+use App\Http\Controllers\ExceptionFeedbackController;
 
 class Handler extends ExceptionHandler
 {
@@ -32,6 +34,8 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        //
+        $this->reportable(function (Exception $e) {
+            return response(ExceptionFeedbackController::error($e), 200);
+         });
     }
 }
